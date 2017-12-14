535cc0a950e233d66eeb27c9932dc178f12abee2
DarkNoys <s-paradox@ro.ru>
2017-12-15 00:48:04 +0300

    Binary

---
+++ CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/renesat/Project/MiniTicTacToe/src/client/client.cpp" "/home/renesat/Project/MiniTicTacToe/CMakeFiles/client.dir/client/client.o"
+  "/home/renesat/Project/MiniTicTacToe/src/client/mainClient.cpp" "/home/renesat/Project/MiniTicTacToe/CMakeFiles/client.dir/client/mainClient.o"
+  "/home/renesat/Project/MiniTicTacToe/src/tictactoe/tictactoe.cpp" "/home/renesat/Project/MiniTicTacToe/CMakeFiles/client.dir/tictactoe/tictactoe.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "src/header"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/renesat/Project/MiniTicTacToe/src/server/server.cpp" "/home/renesat/Project/MiniTicTacToe/CMakeFiles/server.dir/server/server.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "src/header"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
